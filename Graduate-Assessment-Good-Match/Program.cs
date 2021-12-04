aef603f2a2fc21bfb6dcf7d9dc8de4ffd08df781
fverco <francov2468@gmail.com>
2021-12-04 14:49:11 +0200

    Add project files.

---
+++ Graduate-Assessment-Good-Match/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Graduate_Assessment_Good_Match
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
