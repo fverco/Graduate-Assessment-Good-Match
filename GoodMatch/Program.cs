b6574494485022df13ce1b4b99f239f8be7a9e46
fverco <francov2468@gmail.com>
2021-12-05 23:03:17 +0200

    Change use of MatchPlayers method and finish RunMultiInputMode method

--- GoodMatch/Program.cs
+++ GoodMatch/Program.cs
@@ -48,7 +48,16 @@ namespace GoodMatch
 
             Console.WriteLine();
 
-            Console.WriteLine(PlayerMatcher.MatchPlayers(name1, name2));
+            int percentage = PlayerMatcher.MatchPlayers(name1, name2);
+
+            if (percentage != -1)
+            {
+                Console.WriteLine(OutputPercentageMessage(name1, name2, percentage));
+            }
+            else
+            {
+                Console.WriteLine("One or both names contain non-alphabetic characters.");
+            }
         }
 
         /// <summary>
@@ -80,7 +89,8 @@ namespace GoodMatch
                 if (player[1] == "m" && !malePlayerList.Contains(player[0]))
                 {
                     malePlayerList.Add(player[0]);
-                } else
+                }
+                else
                 {
                     if (player[1] == "f" && !femalePlayerList.Contains(player[0]))
                     {
@@ -93,7 +103,55 @@ namespace GoodMatch
 
             sr.Close();
 
-            
+            List<MatchResult> results = new();
+
+            foreach (string male in malePlayerList)
+            {
+                foreach (string female in femalePlayerList)
+                {
+                    MatchResult matchResult = new MatchResult();
+                    int percentage = PlayerMatcher.MatchPlayers(male, female);
+                    matchResult.name1 = male;
+                    matchResult.name2 = female;
+                    matchResult.percentage = percentage;
+                    matchResult.resultMessage = OutputPercentageMessage(male, female, percentage);
+
+                    results.Add(matchResult);
+                }
+            }
+
+            results.Sort((x, y) => y.percentage.CompareTo(x.percentage));
+
+            StreamWriter sw = new("output.txt");
+
+            for (int i = 0; i < results.Count; i++)
+            {
+                sw.WriteLine(results[i].resultMessage);
+            }
+
+            sw.Flush();
+            sw.Close();
+        }
+
+        /// <summary>
+        /// Creates a message for the user about the match percentage between two players.
+        /// </summary>
+        /// <param name="name1"></param>
+        /// <param name="name2"></param>
+        /// <param name="matchPercentage"></param>
+        /// <returns>A string message</returns>
+        static string OutputPercentageMessage(in string name1, in string name2, in int matchPercentage)
+        {
+            string output = $"{name1} matches {name2} {matchPercentage}%";
+
+            if (matchPercentage > 80)
+            {
+                return output + ", good match";
+            }
+            else
+            {
+                return output;
+            }
         }
     }
 }
