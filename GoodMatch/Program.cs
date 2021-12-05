b3976ddade36f458a9331575fdfddc1183ba7782
fverco <francov2468@gmail.com>
2021-12-05 21:19:44 +0200

    Create functions for each input mode and remove unnecessary args parameter

--- GoodMatch/Program.cs
+++ GoodMatch/Program.cs
@@ -5,7 +5,7 @@ namespace GoodMatch
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             string inp = "";
             Console.WriteLine("Welcome to Good Match.");
@@ -19,25 +19,42 @@ namespace GoodMatch
 
             if (inp.ToLower() == "s")
             {
-                string name1, name2;
-
-                Console.WriteLine("Single input mode.\nPlease provide two player names...\n");
-
-                Console.Write("Name 1: ");
-                name1 = Console.ReadLine();
-
-                Console.WriteLine();
-
-                Console.Write("Name 2: ");
-                name2 = Console.ReadLine();
-
-                Console.WriteLine();
-
-                Console.WriteLine(PlayerMatcher.MatchPlayers(name1, name2));
-            } else
-            {
-                Console.WriteLine("Multi output mode.");
+                RunSingleInputMode();
             }
+            else
+            {
+                RunMultiInputMode();
+            }
+        }
+
+        /// <summary>
+        /// Executes Single input mode where only a single pair of players are matched.
+        /// </summary>
+        static void RunSingleInputMode()
+        {
+            string name1, name2;
+
+            Console.WriteLine("Single input mode.\nPlease provide two player names...\n");
+
+            Console.Write("Name 1: ");
+            name1 = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.Write("Name 2: ");
+            name2 = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine(PlayerMatcher.MatchPlayers(name1, name2));
+        }
+
+        /// <summary>
+        /// Executes Multi input mode where multiple pairs of players are matched.
+        /// </summary>
+        static void RunMultiInputMode()
+        {
+            Console.WriteLine("Multi output mode.");
         }
     }
 }
