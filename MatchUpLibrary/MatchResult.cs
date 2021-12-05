61aface2858e11314eea7a91086bec11734abd25
fverco <francov2468@gmail.com>
2021-12-05 23:01:48 +0200

    Add MatchResult struct

---
+++ MatchUpLibrary/MatchResult.cs
@@ -0,0 +1,10 @@
+﻿namespace MatchUpLibrary
+{
+    public struct MatchResult
+    {
+        public string name1;
+        public string name2;
+        public int percentage;
+        public string resultMessage;
+    }
+}
