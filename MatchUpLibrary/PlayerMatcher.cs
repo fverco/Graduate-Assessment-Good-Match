91e9278c388cf50a91770d8bcd80aa53b8f04268
fverco <francov2468@gmail.com>
2021-12-05 11:45:08 +0200

    Create library and class for matching players

---
+++ MatchUpLibrary/PlayerMatcher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatchUpLibrary
+{
+    public static class PlayerMatcher
+    {
+        public static void matchPlayers(string name1, string name2)
+        {
+            string sentance = $"{name1} matches {name2}";
+
+            List<char> sentanceLetters = new List<char>();
+            sentanceLetters.AddRange(sentance);
+
+            foreach (char character in sentanceLetters)
+            {
+                Console.WriteLine(character);
+            }
+        }
+    }
+}
